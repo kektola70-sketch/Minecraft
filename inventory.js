7d977ccc385a680f1da18baf063fcbc2c58adfd7
kektola70-sketch <kektola70@gmail.com>
2026-03-01 13:00:43 +0300

    update inventory.js

--- inventory.js
+++ inventory.js
@@ -2,15 +2,15 @@ class Inventory {
     constructor() {
         this.selectedSlot = 0;
         this.blocks = [
-            { id: 1, color: 0x567d46, name: "Grass" },
-            { id: 2, color: 0x795548, name: "Dirt" },
-            { id: 3, color: 0x888888, name: "Stone" },
-            { id: 4, color: 0xA0522D, name: "Wood" },
-            { id: 5, color: 0xFFD700, name: "Gold" },
-            { id: 6, color: 0xFFFFFF, name: "Iron" },
-            { id: 7, color: 0x0000FF, name: "Water" },
-            { id: 8, color: 0xFF0000, name: "TNT" },
-            { id: 9, color: 0x000000, name: "Bedrock" }
+            { id: 1, color: 0x567d46, name: "Grass" },   // Слот 1
+            { id: 2, color: 0x795548, name: "Dirt" },    // Слот 2
+            { id: 3, color: 0x808080, name: "Stone" },   // Слот 3
+            { id: 4, color: 0xA0522D, name: "Wood" },    // Слот 4
+            { id: 5, color: 0xD2B48C, name: "Iron Ore" },// Слот 5 (Руда)
+            { id: 6, color: 0xFFD700, name: "Gold" },    // Слот 6
+            { id: 7, color: 0x111111, name: "Bedrock" }, // Слот 7
+            { id: 8, color: 0xFF0000, name: "TNT" },     // Слот 8
+            { id: 9, color: 0x0000FF, name: "Water" }    // Слот 9
         ];
         this.initUI();
     }
