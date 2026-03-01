a9fe0b430791c1f052b776f868c87c24949c0897
kektola70-sketch <kektola70@gmail.com>
2026-03-01 13:13:53 +0300

    update inventory.js

--- inventory.js
+++ inventory.js
@@ -2,15 +2,15 @@ class Inventory {
     constructor() {
         this.selectedSlot = 0;
         this.blocks = [
-            { id: 1, color: 0x567d46, name: "Grass" },
-            { id: 2, color: 0x795548, name: "Dirt" },
-            { id: 3, color: 0x808080, name: "Stone" },
-            { id: 4, color: 0xA0522D, name: "Wood" },
-            { id: 5, color: 0xD2B48C, name: "Iron" },
-            { id: 6, color: 0xFFD700, name: "Gold" },
-            { id: 7, color: 0x111111, name: "Bedrock" },
-            { id: 8, color: 0xFF0000, name: "TNT" },
-            { id: 9, color: 0x0000FF, name: "Water" }
+            { id: 1, color: 0x567d46, name: "Grass" },   // 1. Трава
+            { id: 2, color: 0x795548, name: "Dirt" },    // 2. Земля
+            { id: 3, color: 0x808080, name: "Stone" },   // 3. Камень
+            { id: 4, color: 0xA0522D, name: "Wood" },    // 4. Дерево
+            { id: 5, color: 0x228B22, name: "Leaves" },  // 5. Листва (Зеленая)
+            { id: 6, color: 0xF4A460, name: "Sand" },    // 6. Песок
+            { id: 7, color: 0xFFFFFF, name: "Snow" },    // 7. Снег
+            { id: 8, color: 0x111111, name: "Bedrock" }, // 8. Бедрок
+            { id: 9, color: 0x0000FF, name: "Water" }    // 9. Вода
         ];
         this.initUI();
     }
